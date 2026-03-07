a292698b69204f36541d1f1a3f26d093177352d1
Matilda <153890630+Bobateaaa@users.noreply.github.com>
2026-03-07 17:16:38 -0500

    changement de la methode d'importation des fonts

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   base: '/projet-finale-cafe/',
   publicDir: 'public',
   build: {
     outDir: 'dist'
-  },
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/font/*',
-          dest: 'font'
-        }
-      ]
-    })
-  ]
+  }
 });
